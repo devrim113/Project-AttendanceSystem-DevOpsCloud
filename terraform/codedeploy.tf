fdb8f853543fad7b8c9d245d1d3a17ac1d4405d1
Devrim Yugnuk <devrimyugnuk@gmail.com>
2024-02-27 18:10:45 +0100

    Refactored code to have the same indentation standard, added codedeploy terraform.

---
+++ terraform/codedeploy.tf
@@ -0,0 +1,43 @@
+resource "aws_codedeploy_app" "attendance_lambda" {
+  name             = "AttendanceLambda"
+  compute_platform = "Lambda"
+}
+
+resource "aws_codedeploy_deployment_group" "lambda_deployment_group" {
+  app_name              = aws_codedeploy_app.attendance_lambda.name
+  deployment_group_name = "AttendanceLambdaDeploymentGroup"
+  service_role_arn      = aws_iam_role.codedeploy_role.arn
+
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
+  }
+
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+  }
+}
+
+resource "aws_iam_role" "codedeploy_role" {
+  name = "codedeploy_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "codedeploy.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
