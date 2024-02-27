0286743af040c6784f5ac789b1eb9bc0f40396cd
Devrim Yugnuk <devrimyugnuk@gmail.com>
2024-02-27 19:53:10 +0100

    Fixed a lot of styles, fixed the dynamoDB and frontend terraform files to work and can be applied.

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 resource "aws_s3_bucket" "S3_Bucket" {
-  bucket = "code-bucket"
+  bucket = "attendance-frontend-bucket"
 
   tags = {
     Name = "The code bucket"
@@ -13,19 +13,19 @@ resource "aws_s3_bucket" "S3_Bucket" {
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name              = aws_s3_bucket.S3_Bucket.bucket_regional_domain_name
-    origin_id                = local.s3_origin_id
+    domain_name = aws_s3_bucket.S3_Bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
   }
 
   enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
 
-  logging_config {
-    include_cookies = false
-    bucket          = "mylogs.s3.amazonaws.com"
-    prefix          = "myprefix"
-  }
+  # logging_config {
+  #   include_cookies = false
+  #   bucket          = "mylogs.s3.amazonaws.com"
+  #   prefix          = "myprefix"
+  # }
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
@@ -47,49 +47,49 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   # Cache behavior with precedence 0
-#   ordered_cache_behavior {
-#     path_pattern     = "/content/immutable/*"
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-#     target_origin_id = local.s3_origin_id
+  #   ordered_cache_behavior {
+  #     path_pattern     = "/content/immutable/*"
+  #     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+  #     cached_methods   = ["GET", "HEAD", "OPTIONS"]
+  #     target_origin_id = local.s3_origin_id
 
-#     forwarded_values {
-#       query_string = false
-#       headers      = ["Origin"]
+  #     forwarded_values {
+  #       query_string = false
+  #       headers      = ["Origin"]
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+  #       cookies {
+  #         forward = "none"
+  #       }
+  #     }
 
-#     min_ttl                = 0
-#     default_ttl            = 86400
-#     max_ttl                = 31536000
-#     compress               = true
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
+  #     min_ttl                = 0
+  #     default_ttl            = 86400
+  #     max_ttl                = 31536000
+  #     compress               = true
+  #     viewer_protocol_policy = "redirect-to-https"
+  #   }
 
-#   # Cache behavior with precedence 1
-#   ordered_cache_behavior {
-#     path_pattern     = "/content/*"
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
+  #   # Cache behavior with precedence 1
+  #   ordered_cache_behavior {
+  #     path_pattern     = "/content/*"
+  #     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+  #     cached_methods   = ["GET", "HEAD"]
+  #     target_origin_id = local.s3_origin_id
 
-#     forwarded_values {
-#       query_string = false
+  #     forwarded_values {
+  #       query_string = false
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+  #       cookies {
+  #         forward = "none"
+  #       }
+  #     }
 
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#     compress               = true
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
+  #     min_ttl                = 0
+  #     default_ttl            = 3600
+  #     max_ttl                = 86400
+  #     compress               = true
+  #     viewer_protocol_policy = "redirect-to-https"
+  #   }
 
   restrictions {
     geo_restriction {
