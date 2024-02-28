60d5f79a2127de2f7c1e451ddeeeb96913dc829d
Tarik Kada <tarikkada2@gmail.com>
2024-02-28 23:04:29 +0100

    Simplified Database design

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -3,21 +3,20 @@ resource "aws_dynamodb_table" "attendance_table" {
   billing_mode   = "PROVISIONED"
   read_capacity  = 5
   write_capacity = 5
-  hash_key       = "CourseUserDate"
+  hash_key       = "UserID"
   range_key      = "CourseID"
 
-  attribute {
-    name = "CourseUserDate"
-    type = "S"
+  global_secondary_index {
+    name               = "CourseIDUserTypeIndex"
+    hash_key           = "CourseID"
+    range_key = "UserType"
+    read_capacity  = 5
+    write_capacity = 5
+    projection_type    = "ALL"
   }
 
   attribute {
     name = "UserID"
-    type = "S"
-  }
-
-  attribute {
-    name = "UserType"
     type = "S"
   }
 
@@ -27,59 +26,8 @@ resource "aws_dynamodb_table" "attendance_table" {
   }
 
   attribute {
-    name = "Date"
+    name = "UserType"
     type = "S"
-  }
-
-  local_secondary_index {
-    name            = "UserData"
-    projection_type = "ALL"
-    range_key       = "UserID"
-  }
-
-  # Attendance on specific date for all courses
-  local_secondary_index {
-    name            = "Attendance"
-    projection_type = "ALL"
-    range_key       = "Date"
-  }
-
-  local_secondary_index {
-    name            = "UserData"
-    projection_type = "ALL"
-    range_key       = "UserType"
-  }
-
-
-  # Course's attendance on specific date for all students
-  global_secondary_index {
-    name            = "CourseDate"
-    hash_key        = "CourseID"
-    range_key       = "Date"
-    write_capacity  = 5
-    read_capacity   = 5
-    projection_type = "ALL"
-  }
-
-  # To sort teachers from students in course
-  global_secondary_index {
-    name               = "CourseUsers"
-    hash_key           = "CourseID"
-    range_key          = "UserType"
-    write_capacity     = 5
-    read_capacity      = 5
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["UserID"]
-  }
-
-  # User's attendance for specific course
-  global_secondary_index {
-    name            = "UserCourse"
-    hash_key        = "UserID"
-    range_key       = "CourseID"
-    write_capacity  = 5
-    read_capacity   = 5
-    projection_type = "ALL"
   }
 
   tags = {
