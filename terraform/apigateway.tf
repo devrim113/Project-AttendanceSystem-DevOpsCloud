8a8f19a52ed5de9f6080a22987f5b6658d518214
Devrim Yugnuk <devrimyugnuk@gmail.com>
2024-02-28 17:13:37 +0100

    Should now be fixed

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -87,7 +87,7 @@ resource "aws_api_gateway_integration_response" "integration_responses" {
   status_code = aws_api_gateway_method_response.responses[each.key].status_code
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Access-Control-Allow-Origin'",
     "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
