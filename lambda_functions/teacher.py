afa06f400754eda4689e3e9d18a86b863ef9613c
Devrim Yugnuk <devrimyugnuk@gmail.com>
2024-02-28 17:26:44 +0100

    Changed the headers in the response of lambda functions to enable CORS.

--- lambda_functions/teacher.py
+++ lambda_functions/teacher.py
@@ -4,5 +4,10 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda teacher v2!')
+        'headers': {
+            'Access-Control-Allow-Origin': '*',  # Allows requests from any origin
+            'Access-Control-Allow-Methods': 'OPTIONS,GET,POST,PUT,DELETE',  # Adjust based on your needs
+            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Access-Control-Allow-Origin'  # Ensure this matches the headers your client may send
+        },
+        'body': json.dumps('Hello from Lambda!')
     }
