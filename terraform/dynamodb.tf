11f8a95861c9ab43245372ca8abaa91948f9c4ba
Tarik Kada <tarikkada2@gmail.com>
2024-03-01 14:18:28 +0100

    Updated database configuration

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -3,16 +3,44 @@ resource "aws_dynamodb_table" "attendance_table" {
   billing_mode   = "PROVISIONED"
   read_capacity  = 5
   write_capacity = 5
-  hash_key       = "UserID"
-  range_key      = "CourseID"
+  hash_key       = "ItemId"
+  range_key      = "ItemType"
 
   global_secondary_index {
-    name               = "CourseIDUserTypeIndex"
+    name               = "CourseIDITemTypeIndex"
     hash_key           = "CourseID"
-    range_key = "UserType"
+    range_key = "ItemType"
     read_capacity  = 5
     write_capacity = 5
     projection_type    = "ALL"
+  }
+
+  global_secondary_index {
+    name              = "DepartmentIdItemTypeIndex"
+    hash_key          = "DepartmentID"
+    range_key = "ItemType"
+    read_capacity  = 5
+    write_capacity = 5
+    projection_type   = "ALL"
+  }
+
+  global_secondary_index {
+    name              = "UserIdCourseIdIndex"
+    hash_key          = "UserID"
+    range_key = "CourseID"
+    read_capacity  = 5
+    write_capacity = 5
+    projection_type   = "ALL"
+  }
+
+  attribute {
+    name = "ItemId"
+    type = "S"
+  }
+
+  attribute {
+    name = "ItemType"
+    type = "S"
   }
 
   attribute {
@@ -21,12 +49,12 @@ resource "aws_dynamodb_table" "attendance_table" {
   }
 
   attribute {
-    name = "CourseID"
+    name = "DepartmentID"
     type = "S"
   }
 
   attribute {
-    name = "UserType"
+    name = "CourseID"
     type = "S"
   }
 
