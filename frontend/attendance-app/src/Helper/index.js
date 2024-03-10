4b1c5f59fb5389b233b420b1774a0af5c12947f7
Devrim Yugnuk <devrimyugnuk@gmail.com>
2024-03-10 22:30:05 +0100

    Removed the console.log for the cookies as that works fine

--- frontend/attendance-app/src/Helper/index.js
+++ frontend/attendance-app/src/Helper/index.js
@@ -1,8 +1,6 @@
 import Cookies from 'js-cookie';
 function setCookie(name, value, options = { expires: 1, path: '/' }) {
-    console.log(document.cookie)
     Cookies.set(name, value, options);
-    console.log(document.cookie)
 }
 
 function getCookie(name) {
