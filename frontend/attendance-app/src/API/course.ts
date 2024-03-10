be036aa038dbb290e1d272478b6b30ec6ee45ca5
Devrim Yugnuk <devrimyugnuk@gmail.com>
2024-03-10 22:31:01 +0100

    Added the create_course and update_course functions for the courses api file.

--- frontend/attendance-app/src/API/course.ts
+++ frontend/attendance-app/src/API/course.ts
@@ -1,16 +1,16 @@
 import { API_PATH_COURSE, API_SCHEMA, FULL_API_URL } from "../Helper/static";
 
-function get_URL(parameters: { [key: string]: string },method: string, body: { [key: string]: any } | undefined) {
+function get_URL(parameters: { [key: string]: string }, method: string, body: { [key: string]: any } | undefined) {
     // Check if 'func' is in parameters
     if (!('func' in parameters)) {
         throw new Error('func not in parameters');
     }
 
-    let url =  new URL(
+    let url = new URL(
         FULL_API_URL(API_SCHEMA, API_PATH_COURSE),
     )
     url.search = new URLSearchParams(parameters).toString();
-    return ( async () => fetch(url.toString(), {
+    return (async () => fetch(url.toString(), {
         method: method,
         headers: {
             'Content-Type': 'application/json',
@@ -19,27 +19,17 @@ function get_URL(parameters: { [key: string]: string },method: string, body: { [
     }))
 }
 
-export function create_course(ItemId: string, CourseName: string, ItemType: string, DepartmentId: string, Classes: { [key: string]: { [key: string]: string } }) {
+export function create_course(ItemId: string, CourseName: string, DepartmentId: string) {
     return get_URL(
         {
             "func": 'create_course'
-        }, 
+        },
         'PUT',
         {
-            'ItemId': '1',
-            'CourseName': 'Math',
-            'ItemType': 'Course',
-            'DepartmentId': '1',
-            'Classes': {
-                '2022-01-01': {
-                    'from': '09:00',
-                    'to': '12:00'
-                },
-                '2022-01-02': {
-                    'from': '09:00',
-                    'to': '12:00'
-                }
-            }
+            "ItemId": ItemId,
+            "CourseName": CourseName,
+            "DepartmentId": DepartmentId,
+            "Classes": "",
         }
     )
 }
@@ -49,8 +39,23 @@ export function get_course(ItemId: string) {
         {
             "func": 'get_course',
             "ItemId": ItemId
-        }, 
+        },
         'GET',
         undefined
+    )
+}
+
+export function update_course(ItemId: string, CourseName: string, DepartmentId: string, Classes: { [key: string]: { [key: string]: string } }) {
+    return get_URL(
+        {
+            "func": 'update_course'
+        },
+        'POST',
+        {
+            "ItemId": ItemId,
+            "CourseName": CourseName,
+            "DepartmentId": DepartmentId,
+            "Classes": Classes,
+        }
     )
 }
