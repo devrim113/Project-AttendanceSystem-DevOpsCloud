e5d2974fdc8cb3ba66d6cee66db9279e716d8c02
julialfk <j.liemfk@gmail.com>
2024-03-10 16:49:26 +0100

    Added global variable tf file

---
+++ terraform/variable.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "account_id" {
+  description = "AWS account ID"
+  type        = string
+  default     = "590183910477"
+}
+
+variable "api_id" {
+  description = "API Gateway API ID"
+  type        = string
+  default     = "r49e847uie"
+}
