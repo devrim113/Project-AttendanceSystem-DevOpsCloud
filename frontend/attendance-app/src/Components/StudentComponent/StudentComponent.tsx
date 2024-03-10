15ee2489ff940d760728dc6c8c9d55e99691c1a1
Devrim Yugnuk <devrimyugnuk@gmail.com>
2024-03-10 22:33:33 +0100

    Created an interface for the courses with names, a big course interface for complete course information, used states for showing and loading courses. Added new functions for getting student's courses and all courses. Updated the html to display everything correctly. This page is completely done.

--- frontend/attendance-app/src/Components/StudentComponent/StudentComponent.tsx
+++ frontend/attendance-app/src/Components/StudentComponent/StudentComponent.tsx
@@ -1,34 +1,50 @@
 import React, { useEffect, useState } from "react";
 import { getInformation } from "../../Helper/static";
-import { get_student_course_names, get_student_courses, get_student_list } from "../../API/student";
+import { enlist_student_course, get_all_courses, get_student_course_names } from "../../API/student";
+
+// Define a TypeScript interface for the course
+interface Courses {
+    [key: number]: string;
+}
+
+interface bigCourse {
+    Classes: object;
+    CourseName: string;
+    DepartmentId: string;
+    ItemId: string;
+    ItemType: string;
+}
 
 function StudentComponent() {
-    const [courses, setCourses] = useState<object | null>(null);
-    const [allcourses, setAllCourses] = useState<object | null>(null);
+    const [courses, setCourses] = useState<Courses[] | null>(null);
+    const [allcourses, setAllCourses] = useState<bigCourse[] | null>(null);
+    const [loadingCourses, setLoadingCourses] = useState(true);
+    const [loadingAllCourses, setLoadingAllCourses] = useState(true);
+    const username = getInformation()["cognito:username"];
 
-    // UseEffect will run once the component is mounted.
-    // It will get the student's courses and display them in a table.
     useEffect(() => {
-        const username = getInformation()["cognito:username"]
-
         // Get the student's courses
-        get_student_courses(username)().then((response) => {
-            if(response.status === 200) {
-                setCourses(response.json())
-            }
-            if(response.status === 404) {
-                setCourses([{}])
-            }
-        });
-        
-        // Get all the courses
         get_student_course_names(username)().then((response) => {
-            if(response.status === 200) {
-                setAllCourses(response.json())
-            }
-            if(response.status === 404) {
-                setAllCourses([{}])
-            }
+            response.json().then(data => {
+                if (response.status === 200) {
+                    setCourses(data);
+                } else {
+                    setCourses([]);
+                }
+                setLoadingCourses(false);
+            });
+        });
+
+        // Get all the courses
+        get_all_courses()().then((response) => {
+            response.json().then(data => {
+                if (response.status === 200 && data.length > 0) {
+                    setAllCourses(data);
+                } else {
+                    setAllCourses([]);
+                }
+                setLoadingAllCourses(false);
+            });
         });
     }, []);
 
@@ -42,60 +58,105 @@ function StudentComponent() {
             </p>
             <div className="row">
                 <div className="col-12">
-                <div className="card">
-                    <div className="card-header">
-                    <h5 className="card-title">Courses</h5>
-                    <h6 className="card-subtitle text-muted">
-                        These are all the courses you have enrolled in.
-                    </h6>
+                    <div className="card">
+                        <div className="card-header">
+                            <h5 className="card-title">Courses</h5>
+                            <h6 className="card-subtitle text-muted">These are all the courses you have enrolled in.</h6>
+                        </div>
+                        <div className="card-body">
+                            <table className="table table-striped" style={{ width: "100%" }}>
+                                <thead>
+                                    <tr>
+                                        <th>Course Name</th>
+                                        <th>View</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {loadingCourses ? (
+                                        <tr><td>Loading...</td><td></td></tr>
+                                    ) : courses ? (
+                                        Object.entries(courses).map(([course_id, course_name], index) => (
+                                            <tr key={index}>
+                                                <td>{String(course_name)}</td>
+                                                <td>
+                                                    <button className="btn btn-primary" onClick={() => window.location.href = `/studentattendance?course_id=${encodeURIComponent(String(course_id))}&course_name=${encodeURIComponent(course_name.toString())}`}>View</button>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    ) : (
+                                        <tr><td>No courses found.</td><td></td></tr>
+                                    )}
+                                </tbody>
+
+                            </table>
+                        </div>
                     </div>
-                    <div className="card-body">
-                    <table
-                        id="datatables-responsive"
-                        className="table table-striped"
-                        style={{ width: "100%" }}
-                    >
-                        <thead>
-                        <tr>
-                            <th>Course Name</th>
-                            <th>View</th>
-                        </tr>
-                        </thead>
-                        <tbody id="data-student-courses"></tbody>
-                    </table>
-                    </div>
-                </div>
                 </div>
             </div>
             <div className="row">
                 <div className="col-12">
-                <div className="card">
-                    <div className="card-header">
-                    <h5 className="card-title">Enroll in Course</h5>
-                    <h6 className="card-subtitle text-muted">
-                        Choose one of the courses to enroll in.
-                    </h6>
+                    <div className="card">
+                        <div className="card-header">
+                            <h5 className="card-title">Enroll in Course</h5>
+                            <h6 className="card-subtitle text-muted">Choose one of the courses to enroll in.</h6>
+                        </div>
+                        <div className="card-body">
+                            <table className="table table-striped" style={{ width: "100%" }}>
+                                <thead>
+                                    <tr>
+                                        <th>Course Name</th>
+                                        <th>Enroll</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {loadingAllCourses ? <tr><td>Loading...</td></tr> : allcourses && allcourses.length > 0 ? allcourses.map((course, index) => (
+                                        <tr key={index}>
+                                            <td>{course.CourseName}</td>
+                                            <td>
+                                                <button
+                                                    className="btn btn-primary"
+                                                    onClick={async () => {
+                                                        try {
+                                                            const courseId = username + '#' + course.ItemId;
+
+                                                            const fetchFunction = enlist_student_course(courseId, username, course.ItemId);
+                                                            const response = await fetchFunction();
+
+                                                            if (!response.ok) {
+                                                                const result = await response.json();
+                                                                if (result === "Request not finished succesfully: The conditional request failed") {
+                                                                    alert("You are already enrolled in this course.");
+                                                                    return;
+                                                                }
+                                                                throw new Error(`HTTP error! status: ${response.status}`);
+                                                            }
+
+                                                            const result = await response.json();
+                                                            if (result === "Record created successfully.") {
+                                                                alert('Enrolled successfully.');
+                                                                window.location.reload();
+                                                            } else {
+                                                                alert(`Error: ${JSON.stringify(result)}`);
+                                                            }
+                                                        } catch (error) {
+                                                            console.error(error);
+                                                            alert(`Error: ${(error as Error).message}`);
+                                                        }
+                                                    }}
+                                                >
+                                                    Enroll
+                                                </button>
+                                            </td>
+
+                                        </tr>
+                                    )) : <tr><td>No courses available for enrollment.</td></tr>}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-                    <div className="card-body">
-                    <table
-                        id="datatables-responsive-2"
-                        className="table table-striped"
-                        style={{ width: "100%" }}
-                    >
-                        <thead>
-                        <tr>
-                            <th>Course Name</th>
-                            <th>Enroll</th>
-                        </tr>
-                        </thead>
-                        <tbody id="data-enroll-courses"></tbody>
-                    </table>
-                    </div>
-                </div>
                 </div>
             </div>
-        </div>
-
-    )
+        </div >
+    );
 }
-export default StudentComponent
+export default StudentComponent;
