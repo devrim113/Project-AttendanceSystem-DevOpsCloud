c8386a9e6aa9690bdc155e70dbfdc2da1cd6a4c7
Devrim Yugnuk <devrimyugnuk@gmail.com>
2024-03-15 10:40:39 +0100

    Moved all the variables to the variable.tf file.

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -9,3 +9,34 @@ variable "account_id" {
   type        = string
   default     = "590183910477"
 }
+
+# Defining the paths that have to be created for the API Gateway.
+variable "to_define_paths" {
+  type    = list(string)
+  default = ["admin", "teacher", "course", "department", "student", "cognito"]
+}
+
+# Defining the methods that have to be created for the API Gateway.
+variable "to_define_methods" {
+  type    = list(string)
+  default = ["GET", "OPTIONS", "PUT", "POST", "DELETE", "HEAD"]
+}
+
+# Defining the lambda functions and their handlers.
+variable "lambda_functions" {
+  type = map(string)
+  default = {
+    "student"    = "student.lambda_handler",
+    "admin"      = "admin.lambda_handler",
+    "teacher"    = "teacher.lambda_handler",
+    "course"     = "course.lambda_handler",
+    "department" = "department.lambda_handler",
+    "cognito"    = "cognito.lambda_handler",
+  }
+}
+
+# Defining the s3 origin id for the CloudFront distribution.
+variable "s3_origin_id" {
+  type    = string
+  default = "s3CodeBucketOrigin"
+}
