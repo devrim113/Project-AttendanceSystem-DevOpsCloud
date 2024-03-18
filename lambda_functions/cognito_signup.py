acb28b020f1d97c46f25d04f93eefdc3532e1f0d
Ryan <ryanbriso@gmail.com>
2024-03-18 08:41:22 +0100

    Update cognito_signup.py

--- lambda_functions/cognito_signup.py
+++ lambda_functions/cognito_signup.py
@@ -8,28 +8,28 @@ table = dynamodb.Table('AllData')
 
 def lambda_handler(event, context):
     user_id = event['userName']
-    try:
-        name = event['request']['userAttributes'].get('name', "Unknown")
-        userPoolId = event['userPoolId']
-        
-        client = boto3.client('cognito-idp')
-        response_Add_To_Group = client.admin_add_user_to_group(
-            UserPoolId=userPoolId,
-            Username=user_id,
-            GroupName="Students"
-        )
+    # try:
+    name = event['request']['userAttributes'].get('name', "Unknown")
+    userPoolId = event['userPoolId']
+    
+    client = boto3.client('cognito-idp')
+    response_Add_To_Group = client.admin_add_user_to_group(
+        UserPoolId=userPoolId,
+        Username=user_id,
+        GroupName="Students"
+    )
 
-        response = table.put_item(
-            Item={
-                'ItemId': user_id,
-                'UserName': name,
-                'ItemType': "Students"
-            },
-            ConditionExpression='attribute_not_exists(ItemId) AND attribute_not_exists(ItemType)'
-        )
-    except Exception as e:
-        print(f"Error processing user {user_id}: {str(e)}")
-        e["error"] = str(e)
+    response = table.put_item(
+        Item={
+            'ItemId': user_id,
+            'UserName': name,
+            'ItemType': "Students"
+        },
+        ConditionExpression='attribute_not_exists(ItemId) AND attribute_not_exists(ItemType)'
+    )
+    # except Exception as e:
+    #     print(f"Error processing user {user_id}: {str(e)}")
+    #     e["error"] = str(e)
 
     # Always return the event object in its expected format for Cognito triggers
     return event
